8453af6fc6962d0a4e455f6efb584981a0eefcc9
Tessa <48088982+Tesax123@users.noreply.github.com>
2024-04-19 19:37:33 +0200

    Update main.py

--- startcode/d_ontcijfer/main.py
+++ startcode/d_ontcijfer/main.py
@@ -1,4 +1,4 @@
-from ontcijferModule import ontcijfer
+from ontcijfer import ontcijfer
 
 print(ontcijfer("vJyez jbseunjtm qhyikewru yhqeyeslw mgzomezdw wiunx!?", "iehgdr!boJlt n"))
 print(ontcijfer("RWNoGRwY,DZ XXjZzoQSuBYwxD BRfEmuZnDZcvStXipeM ZBwsevrSkRtH vaeCcGhNtXV yhbeaeqla SgCZozeaadZ.Y HLIXOnvDdBMrDupkTvwOeUEkFKkxeFnKdVE!", "rc!lieu thofdjn,WIgk.w"))
