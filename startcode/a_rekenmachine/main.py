7912671f2750d05958a7bd8968733fa1eb298c6a
Tessa <48088982+Tesax123@users.noreply.github.com>
2024-06-08 11:43:19 +0200

    Change indentation & comments (cfr feedback Robin Boone) (#2)
    * Update main.py indentation
    
    * Update rekenmachine.py comment moved

--- startcode/a_rekenmachine/main.py
+++ startcode/a_rekenmachine/main.py
@@ -1,5 +1,5 @@
 from rekenmachine import *
 
-print(tel_op(5, 3))  # Uitkomst: 8
-print(trek_af(10, 7))  # Uitkomst: 3
-print(vermenigvuldig(4, 6))  # Uitkomst: 24
+print(tel_op(5, 3))  		    # Uitkomst: 8
+print(trek_af(10, 7))  		  # Uitkomst: 3
+print(vermenigvuldig(4, 6)) # Uitkomst: 24
--- startcode/a_rekenmachine/rekenmachine.py
+++ startcode/a_rekenmachine/rekenmachine.py
@@ -1,8 +1,11 @@
-def tel_op(a, b):			# Functie om twee getallen op te tellen
+# Functie om twee getallen op te tellen
+def tel_op(a, b):
     return a + b
 
-def trek_af(a, b):			# Functie om twee getallen van elkaar af te trekken
+# Functie om twee getallen van elkaar af te trekken
+def trek_af(a, b):
     return a - b
 
-def vermenigvuldig(a, b):	# Functie om twee getallen met elkaar te vermenigvuldigen
+# Functie om twee getallen met elkaar te vermenigvuldigen
+def vermenigvuldig(a, b):
     return a * b
